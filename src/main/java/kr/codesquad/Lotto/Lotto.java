ddae67b5d1e30a46b16e027c5a522da9a97ea9d5
maltepoo <jaengpin@gmail.com>
2023-01-14 14:21:58 +0900

    refactor: input, print 클래스 분리

--- src/main/java/kr/codesquad/Lotto/Lotto.java
+++ src/main/java/kr/codesquad/Lotto/Lotto.java
@@ -1,24 +1,27 @@
 package kr.codesquad.Lotto;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Lotto {
-    private final static List<Integer> LOTTO_NUMBERS = Arrays.asList(
-            1, 2, 3, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 29, 20,
-            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37,
-            38, 39, 40, 41, 42, 43, 44, 45
-    );
+    private final static int LOTTO_PRICE = 1000;
+    private final static int LOTTO_COUNT = 6;
+    private static ArrayList<Integer> LOTTO_NUMBERS;
 
+    public Lotto() {
+        ArrayList<Integer> numbers = new ArrayList<>();
+        for (int i = 1; i < 46; i++) {
+            numbers.add(i);
+        }
+        LOTTO_NUMBERS = numbers;
+    }
 
     public List<Integer> getLottoNumbers() {
         Collections.shuffle(LOTTO_NUMBERS);
-
-        List<Integer> numbers = LOTTO_NUMBERS.stream().limit(6).sorted().collect(Collectors.toList());
-        System.out.println(numbers);
-
-        return numbers;
+        List<Integer> generated = LOTTO_NUMBERS.stream().limit(6).sorted().collect(Collectors.toList());
+        System.out.println(generated);
+        return generated;
     }
 }
--- src/main/java/kr/codesquad/Lotto/LottoGame.java
+++ src/main/java/kr/codesquad/Lotto/LottoGame.java
@@ -1,54 +1,77 @@
 package kr.codesquad.Lotto;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
 public class LottoGame {
+    private static final Lotto lotto = new Lotto();
+    private static final LottoInput lottoInput = new LottoInput();
+    private static final LottoPrints lottoPrints = new LottoPrints();
+
+    private static int budget;
+    private static int lottoCount;
 
     public void play() {
         // 로또 예산 입력받기
-        int budget = getBudget();
-        int lottoCount = getLottoCount(budget);
+        budget = lottoInput.getBudget();
+        lottoCount = lottoInput.getLottoCount(budget);
 
         // lotto count개 랜덤로또 생성
         ArrayList<List<Integer>> bought = purchaseLotto(lottoCount);
 
         // 당첨번호 입력받기
-        List<Integer> win = getWinNumbers();
+        List<Integer> winNumbers = lottoInput.getWinNumbers();
 
         // 당첨통계 & 표시
-        List<Integer> result = matchResult(win ,bought);
-        printResult(result);
+        lottoPrints.printStatics();
+        List<Integer> result = matchResult(winNumbers ,bought);
+        lottoPrints.printResult(result);
 
         // 총 수익률 표시
-        System.out.print("총 수익률은 ");
-        System.out.print(profitRate(lottoCount*1000, result.get(3), result.get(4), result.get(5), result.get(6)));
-        System.out.println("%입니다.");
+        lottoPrints.printTotalProfilt(lottoCount, result);
     }
 
-    public int getBudget() {
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("구입금액을 입력해 주세요.");
-        return scanner.nextInt();
-    }
-
-    public int getLottoCount(int budget) {
-        System.out.println(budget / 1000 + "개를 구매했습니다.");
-        return budget / 1000;
-    }
-
-    public ArrayList<List<Integer>> purchaseLotto(int cnt) {
+    private ArrayList<List<Integer>> purchaseLotto(int cnt) {
         ArrayList<List<Integer>> lottos = new ArrayList<>();
         while (cnt > 0) {
-            Lotto lotto = new Lotto();
             lottos.add(lotto.getLottoNumbers());
             cnt--;
         }
         return lottos;
     }
 
-    public List<Integer> getWinNumbers() {
+    private List<Integer> matchResult(List<Integer> win, ArrayList<List<Integer>> bought) {
+        List<Integer> result = Arrays.asList(0, 0, 0, 0, 0, 0, 0);
+
+        for (List<Integer> integers : bought) {
+            int matchCount = verifyNumbers(win, integers);
+            int countValue = result.get(matchCount);
+
+            result.set(matchCount, countValue + 1);
+        }
+        return result;
+    }
+
+    private int verifyNumbers(List<Integer> win, List<Integer> lotto) {
+        return (int) lotto.stream().filter(win::contains).count();
+    }
+
+}
+
+class LottoInput {
+
+    int getBudget() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("구입금액을 입력해 주세요.");
+        return scanner.nextInt();
+    }
+
+    int getLottoCount(int budget) {
+        System.out.println(budget / 1000 + "개를 구매했습니다.");
+        return budget / 1000;
+    }
+
+    List<Integer> getWinNumbers() {
         System.out.println();
         System.out.println("당첨 번호를 입력해 주세요.");
 
@@ -57,41 +80,34 @@ public class LottoGame {
         List<Integer> winNumbers = new ArrayList<>();
         String[] inputNumbers = scanner.nextLine().split(", ");
 
-        for (String i: inputNumbers) winNumbers.add(Integer.parseInt(i));
+        for (String i : inputNumbers) winNumbers.add(Integer.parseInt(i));
         scanner.close();
 
         return winNumbers;
     }
+}
 
-    public List<Integer> matchResult(List<Integer> win, ArrayList<List<Integer>> bought) {
+class LottoPrints {
+    void printStatics() {
         System.out.println();
         System.out.println("당첨 통계");
         System.out.println("---------");
-
-        List<Integer> result = Arrays.asList(0, 0, 0, 0, 0, 0, 0);
-
-        for (List<Integer> integers : bought) {
-            int matchCount = checkNumbers(win, integers);
-            int countValue = result.get(matchCount);
-
-            result.set(matchCount, countValue + 1);
-        }
-
-        return result;
     }
 
-    private void printResult(List<Integer> result) {
+    void printResult(List<Integer> result) {
         System.out.println("3개 일치 (5000원) - " + result.get(3) + "개");
         System.out.println("4개 일치 (50000원) - " + result.get(4) + "개");
         System.out.println("5개 일치 (1500000원) - " + result.get(5) + "개");
         System.out.println("6개 일치 (2000000000원) - " + result.get(6) + "개");
     }
 
-    public int checkNumbers(List<Integer> win, List<Integer> lotto) {
-        return (int) lotto.stream().filter(win::contains).count();
+    void printTotalProfilt(int lottoCount, List<Integer> result) {
+        System.out.print("총 수익률은 ");
+        System.out.print(profitRate(lottoCount*1000, result.get(3), result.get(4), result.get(5), result.get(6)));
+        System.out.println("%입니다.");
     }
 
-    public int profitRate(int money, int three, int four, int five, int six) {
+    private int profitRate(int money, int three, int four, int five, int six) {
         int profit = (three * 5000) + (four * 50000) + (five * 1500000) + (six * 2000000000);
         return (profit - money) / money * 100;
     }
